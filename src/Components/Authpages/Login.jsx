dbcea0ec74c5954d94e3844017edb307ffcfec13
Likhith Sathish <likithlik54@gmail.com>
2026-03-06 18:38:16 +0530

    header changes

--- src/Components/Authpages/Login.jsx
+++ src/Components/Authpages/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
       bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700">
 
         <h1 className="text-center text-xl font-semibold text-gray-800 dark:text-gray-100 mb-2">
-          DevTinder
+          Likith's APPs
         </h1>
 
         <h2 className="text-lg font-medium text-gray-600 dark:text-gray-300 mb-4">
